a51a4630f55148d9e4b979518876b449d1348eb0
Kcepriu <kcepriu@gmail.com>
2022-11-27 15:08:23 +0200

    ended task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,33 @@
+const formLoginEl = document.querySelector('.login-form');
 
+formLoginEl.addEventListener('submit', onSubmetFormLogin);
+
+function onSubmetFormLogin(event) {
+  event.preventDefault();
+
+  const formDataObj = converFormDataToObject(new FormData(event.currentTarget));
+
+  if (!isAllFieldsAreFilled(formDataObj)) {
+    alert('Всі поля повинні бути заповнені!');
+    return;
+  }
+
+  event.currentTarget.reset();
+
+  console.log(formDataObj);
+}
+
+function converFormDataToObject(formData) {
+  const formDataObj = {};
+  formData.forEach((value, key) => (formDataObj[key] = value));
+
+  return formDataObj;
+}
+
+function isAllFieldsAreFilled(formDataObj) {
+  for (const elem in formDataObj) {
+    if (!formDataObj[elem]) return false;
+  }
+
+  return true;
+}
