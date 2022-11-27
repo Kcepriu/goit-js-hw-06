cdb17d5a987d246d109711ccacc2e059f452dd6a
Kcepriu <kcepriu@gmail.com>
2022-11-27 15:15:10 +0200

    ended task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColorElem = document.querySelector('.change-color');
+const textColorEl = document.querySelector('.color');
+
+btnChangeColorElem.addEventListener('click', onCliclBtnChangeColorElem);
+
+function onCliclBtnChangeColorElem() {
+  const newBackgtoundColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = newBackgtoundColor;
+  textColorEl.textContent = newBackgtoundColor;
+}
